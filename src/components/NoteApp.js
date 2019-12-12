7f9e85fe9186a457d81a2727765ecd31a08965fc
Jag Chumber <admin@loveforweb.com>
2019-12-12 11:15:40 +0000

    added useContext hook

--- src/components/NoteApp.js
+++ src/components/NoteApp.js
@@ -2,13 +2,10 @@ import React, { useEffect, useReducer } from 'react';
 import notesReducer from '../reducers/notes';
 import NoteList from './NoteList';
 import AddNoteForm from './AddNoteForm';
+import NotesContext from '../context/notes-context';
 
 const NoteApp = () => {
   const [notes, dispatch] = useReducer(notesReducer, []);
-
-  const removeNote = title => {
-    dispatch({ type: 'REMOVE_NOTE', title });
-  };
 
   // componentDidMount due to empty []
   useEffect(() => {
@@ -24,11 +21,11 @@ const NoteApp = () => {
   }, [notes]);
 
   return (
-    <div>
+    <NotesContext.Provider value={{ notes, dispatch }}>
       <h1>Notes</h1>
-      <NoteList notes={notes} removeNote={removeNote} />
+      <NoteList />
       <AddNoteForm dispatch={dispatch} />
-    </div>
+    </NotesContext.Provider>
   );
 };
 
