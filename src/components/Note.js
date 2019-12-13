7b51ee8c96a6ee3d97308bd3a5842dbd27c48475
Jag Chumber <admin@loveforweb.com>
2019-12-13 16:22:05 +0000

    Added custom hook

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react';
 import NotesContext from '../context/notes-context';
+import useMousePosition from '../hooks/useMousePosition';
 
 const Note = ({ note }) => {
   const { dispatch } = useContext(NotesContext);
+  const position = useMousePosition();
 
   const removeNote = title => {
     dispatch({ type: 'REMOVE_NOTE', title });
@@ -12,6 +14,9 @@ const Note = ({ note }) => {
     <div key={note.title}>
       <h3>{note.title}</h3>
       <p>{note.body}</p>
+      <p>
+        {position.x}, {position.y}
+      </p>
       <button onClick={() => removeNote(note.title)}>x</button>
     </div>
   );
